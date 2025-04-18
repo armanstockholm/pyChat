fc2b98d8d666797b981b0dd105b9d594c5bd7ab0
Arman Khosravi <armankhosravi@hotmail.com>
2025-04-18 19:54:15 +0200

    Show DB in chat

--- app.py
+++ app.py
@@ -20,9 +20,19 @@ def send_message():
     if entry.get():
         messages_col.insert_one({"text": entry.get()})
         entry.delete(0, tk.END)
+
+def fetch_messages():
+    global message_label
+    messages = messages_col.find().sort("_id")
+    message_label.config(text="Messages: \n " + "\n".join(f"- {m["text"]}" for m in messages))
+    root.after(2000, fetch_messages)
     
     
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
+message_label = tk.Label(root, text="Messages:" , justify="left")
+message_label.pack()
+
+fetch_messages()
 root.mainloop()
